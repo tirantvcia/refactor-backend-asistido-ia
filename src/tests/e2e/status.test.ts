ab43a5da27241fba502fcd6a343e60b296a56afd
tirantvcia <tirantvcia@gmail.com>
2025-12-14 00:36:17 +0100

    green: creates a new order successfully

--- src/tests/e2e/status.test.ts
+++ src/tests/e2e/status.test.ts
@@ -28,3 +28,36 @@ describe('Status endpoint', () => {
         expect(response.body).toEqual({ status: 'ok' });
     });
 });
+
+describe('should create an order', () => {
+    let server : Server;
+    beforeAll(() => {
+        const DB_URL = process.env.MONGODB_URI as string;
+        const PORT = process.env.PORT as string;
+        console.log('Test Environment Variables: ' + DB_URL + ',' + PORT);
+        server = createServer(DB_URL,  Number(PORT));
+    }
+    );
+    afterAll(() => {
+        server.close();
+    });
+    
+
+    it('creates a new order', async () => {
+        const newOrder = {
+            items: [
+                {
+                    productId: 'prod123',
+                    quantity: 1,
+                    price: 100
+                }
+            ]  ,
+            shippingAddress: '123 Test St, Test City, TX 12345'
+
+        }
+        const response = await request(server).post('/orders').send(newOrder);
+        expect(response.status).toBe(200);
+        expect(response.text).toBe(`Order created with total: 100`);
+        
+    });
+});
